6cc78f6bd083d78612312ecb349f971c55543bd4
jihed-chalghaf <jihed.chalghaf@insat.u-carthage.tn>
2022-11-08 13:09:21 +0100

    Use `hardhat-gas-reporter` in the tests.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false,
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
 };
 
 export default config;
